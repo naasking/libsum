5f09ac7a9e8659aaf34a5651ce9cf20e8e7c2df0
Sandro Magi <naasking@gmail.com>
2010-09-19 16:41:34 -0400

    -initial import

---
+++ libsum/libsum.h
@@ -0,0 +1,38 @@
+/*
+ * Disjoint unions, aka sum types, and pattern matching for C.
+ */
+
+#include <memory.h>
+#include <stdlib.h>
+
+/* declare a sum type, which externally just looks like a pointer
+ * to an enum */
+#define SUM(name) typedef enum name* name; enum name
+
+/* declare a concrete instance of the sum, which declares its structure 
+ * and constructors */
+#define CASE(sum, ctor) struct ctor
+//#define CASE(sum, ctor) struct ctor##_outer { enum sum _tag; struct ctor data; }; \
+// static sum _new_##ctor##(struct ctor values) { \
+//   struct ctor##_outer * envelope = (struct ctor##_outer*)malloc(sizeof(struct ctor##_outer)); \
+//   envelope->_tag = ctor; envelope->data = values; return (sum)envelope; \
+// } \
+// struct ctor
+#define CTOR(ctor) struct { int _tag; struct ctor data; }
+
+/* create a new instance of the given sum */
+/* alternately, could use __VA_ARGS__ for initializer */
+#define LET(var, ctor, ...) do { \
+  CTOR(ctor) __tmp = { ctor, { __VA_ARGS__ } }; \
+  var = NEW(ctor); } while (0)
+
+#define NEW(ctor) memcpy( (CTOR(ctor)*)malloc(sizeof(CTOR(ctor))), &__tmp, sizeof(__tmp))
+
+/* deconstruct a sum type */
+#define MATCH(X) int* MATCH_needs_new_scope = (int*)(X); switch(*MATCH_needs_new_scope) {
+
+/* match a particular constructor */
+#define AS(ctor, var) break; } case ctor##: { struct ctor* var = (struct ctor*)MATCH_needs_new_scope++;
+
+/* match any constructor */
+#define MATCHANY break; } default:
---
+++ libsum/tests.c
@@ -0,0 +1,39 @@
+
+#include <stdio.h>
+#include "libsum.h"
+
+SUM(foo) {
+  foo_one,
+  foo_two,
+};
+
+CASE(foo, foo_one) { int i; char c; };
+CASE(foo, foo_two) { double d; };
+
+//CASE(foo, foo_two) {
+//  double d;
+//} ENDCASE
+//CASE(foo, foo_two, { double d; });
+//CASE(foo, foo_two, { 
+//  double d;
+//});
+
+int main(int argc, char** argv) {
+	//LET(foo, f, foo_one, 3, 'g');
+	foo f;
+	LET(f, foo_one, 3, 'g');
+
+	/*foo x;
+	LET(x, foo_two, { 4.567 });*/
+	{
+	MATCH(f) {
+	  AS(foo_one, y) printf("foo_one: %d, %c\n", y->i, y->c);
+	  AS(foo_two, y) printf("foo_two: %d\n", y->d);
+	  MATCHANY
+		fprintf(stderr, "No such case!");
+		exit(1);
+	}
+	}
+	getc(stdin);
+	return 0;
+}
