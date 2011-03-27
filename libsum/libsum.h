264fdb94e8f3c46b7cf84c6951fdfac46a2ff46f
Sandro Magi <naasking@gmail.com>
2011-03-27 09:54:39 -0400

    -document a future enhancement to make tag header overridable -allow only 256 constructor tags, and reserve the remainder for the struct size -document a possible future enhancement for open sums

--- libsum/libsum.h
+++ libsum/libsum.h
@@ -9,6 +9,20 @@
 #include <stdlib.h>
 #include <stddef.h>
 
+/* FIXME: make tag type user-definable, so language implementations
+ * can define GC bits, struct size, etc. Possibly by factoring out
+ * tag structure to a separate file?
+ */
+
+/* FIXME: consider sum extensions, such as adding a new case that never
+ * existed before. Technically, any clients can define new cases by simply
+ * calling providing new values not in the original enum. These will all
+ * fail on MATCHANY, so perhaps we can provide an optional REDIRECT or DELEGATE
+ * operation for "open recursion" ala dispatch. We would then have closed
+ * and open sum types. This only works if new sum values don't conflict of
+ * course.
+ */
+
 /* declare a sum type, which externally just looks like a pointer
  * to an enum */
 #define SUM(name) typedef enum name* name; enum name
@@ -17,7 +31,7 @@
  * and constructors */
 #define CASE(sum, ctor) struct ctor
 
-#define CTOR(ctor) struct { int tag; struct ctor data; }
+#define CTOR(ctor) struct { unsigned tag; struct ctor data; }
 
 /* create a new instance of the given sum */
 #define LET(var, ctor, ...) MAKE(var, ctor, malloc, __VA_ARGS__)
@@ -26,12 +40,12 @@
 #define MAKE(var, ctor, malloc, ...) do { \
   struct ctor __tmp = { __VA_ARGS__ }; \
   CTOR(ctor)* __tmpp = (CTOR(ctor)*)malloc(sizeof(CTOR(ctor))); \
-  __tmpp->tag = ctor; \
+  __tmpp->tag = sizeof(struct ctor) << 8 | ctor; \
   __tmpp->data = __tmp; \
   var = &(__tmpp->tag); } while (0)
 
 /* deconstruct a sum type */
-#define MATCH(X) int* MATCH_needs_new_scope = (int*)(X); switch(*MATCH_needs_new_scope) {
+#define MATCH(X) int* MATCH_needs_new_scope = (int*)(X); switch(*MATCH_needs_new_scope & 0xFF) {
 
 /* match a particular constructor */
 #define AS(ctor, var) break; } case ctor##: { struct ctor* var = FROM(ctor, MATCH_needs_new_scope);
